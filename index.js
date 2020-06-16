e320eba2de96daa60a03cd87eab72e51cc817aa6
MilindKulkarni12 <milindrk12@gmail.com>
2020-06-16 21:59:23 +0530

    Movies Added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 
 const genres = require('./routes/genres');
-const customer = require('./routes/customers');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 
 mongoose.connect('mongodb://localhost/movie_rental', { 
         useNewUrlParser: true, 
@@ -16,7 +17,7 @@ mongoose.connect('mongodb://localhost/movie_rental', {
 app.use(express.json());
 
 app.use('/api/genres', genres);
-app.use('/api/customers',customer);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
-const port = process.env.PORT || 3005;
-app.listen(port, ()=>console.log("Listening on port 3005..."));
+app.listen(3000, ()=>console.log("Listening on port 3000..."));
