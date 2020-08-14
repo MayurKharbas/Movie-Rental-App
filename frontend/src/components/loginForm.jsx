3c561d056a248df766adf61b19322d9e48a49d21
Vishwesh Chaudhari <Vishwesh.Chaudhari@ibm.com>
2020-08-14 19:38:02 +0530

    Added login, logout functionality.

--- frontend/src/components/loginForm.jsx
+++ frontend/src/components/loginForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import auth from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -17,12 +19,25 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {
-    // Call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await auth.login(data.username, data.password);
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
